d491d2ffc0419394487cd9df36c5d6144b0df8d6
maddu rama bhadra rao <ramabhadrarao.maddu@yahoo.com>
2022-07-11 11:46:10 +0530

    Update sendattendance_excel.py

--- sendattendance_excel.py
+++ sendattendance_excel.py
@@ -83,7 +83,7 @@ if check_password():
                 if not data.empty:     
                     for index,x in data.iterrows():
                         if int(x[7]) == 0:
-                                str1="http://bulkwhatsapp.live/wapp/api/send?apikey=f98c13a5259f4cc1ae8822260ad701d5&mobile="
+                                str1="http://bulkwhatsapp.live/wapp/api/send?apikey=511b46864fac4a3f96a50be0fc06d8d0&mobile="
                                 str2=str(x[4])
                                 str3="&msg=""\"Dear Parent,  %0a Your son or daughter :"
                                 str5=str(x[0])
